5283f5332044055582e264ccc71e50d75070bfba
donaldit <ii04071776@gmail.com>
2022-02-17 23:42:20 +0500

    2 attempt

--- solution.go
+++ solution.go
@@ -12,7 +12,7 @@ func CalcSquare(sideLen float64, sidesNum int) float64 {
 	if sidesNum == 4 {
 		return math.Pow(sideLen, 2) 
 	} else if sideNum == 3 {
-		return a*a*math.Sqrt(3) / 4
+		return math.Pow(sideLen, 2) * math.Sqrt(3) / 4
 	} else if sideNum == 0 {
 		return math.Pow(sideLen, 2) * math.Pi / 4
 	}
